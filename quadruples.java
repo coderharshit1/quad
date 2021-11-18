4c54d5062b6c26fc499f2593d1be94d1a582a1d4
coderharshit1 <93389273+coderharshit1@users.noreply.github.com>
2021-11-18 21:09:30 +0530

    Add files via upload

---
+++ quadruples.java
@@ -0,0 +1,34 @@
+class Main
+{
+    
+    public static boolean hasQuadruplet(int[] nums, int n, int target, int count)
+    {
+        
+        if (target == 0 && count == 4) {
+            return true;
+        }
+ 
+        
+        if (count > 4 || n == 0) {
+            return false;
+        }
+ 
+      
+ 
+        return hasQuadruplet(nums, n - 1, target - nums[n - 1], count + 1) ||
+                        hasQuadruplet(nums, n - 1, target, count);
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] nums = { 2, 7, 4, 0, 9, 5, 1, 3 };
+        int target = 20;
+ 
+        if (hasQuadruplet(nums, nums.length, target, 0)) {
+            System.out.println("Quadruplet exists");
+        }
+        else {
+            System.out.println("Quadruplet Doesn't Exist");
+			}
+			}
+			}
